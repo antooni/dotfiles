f963507386df5e69f42df2141d4220214decf3a5
Antoni Pawlak <antoni.pawlak@pm.me>
2022-06-25 14:45:55 +0200

    Update .zsh-install.sh

--- .zsh-install.sh
+++ .zsh-install.sh
@@ -4,14 +4,14 @@ sudo apt-get update
 sudo apt-get install zsh -y
 zsh
 
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
+# wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+# sh install.sh
 
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
-echo ".cfg" >> .gitignore
+# alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+# config config --local status.showUntrackedFiles no
+# echo ".cfg" >> .gitignore
 
-git clone --bare https://github.com/antooni/dotfiles $HOME/.cfg
-config checkout
+# git clone --bare https://github.com/antooni/dotfiles $HOME/.cfg
+# config checkout
 
-source ~/.zshrc
+# source ~/.zshrc
