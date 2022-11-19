5104d864365c540f58d7a256c8fd252d9878f955
Antoni Pawlak <antoni.pawlak@pm.me>
2022-11-19 21:17:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ZSH
-cd ~
+cd $HOME
 sudo apt-get update
 sudo apt-get install zsh -y
 sudo chsh -s $(which zsh)
@@ -9,7 +9,7 @@ sudo chsh -s $(which zsh)
 sudo rm -rf ohmyzsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git
 
-rm ~/.zshrc
+rm .zshrc
 wget https://raw.githubusercontent.com/antooni/dotfiles/master/.zshrc
 
 exec zsh
