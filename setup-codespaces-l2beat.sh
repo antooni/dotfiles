3077c140b5249416ffd88f45b7704a195b8c728e
Antoni Pawlak <antoni.pawlak@pm.me>
2025-01-13 12:04:41 +0100

    Delete setup-codespaces-l2beat.sh

--- setup-codespaces-l2beat.sh
+++
@@ -1,4 +0,0 @@
-sh -c "$(wget -O- https://raw.githubusercontent.com/antooni/dotfiles/master/install.sh)"
-yarn install
-psql -h localhost postgres -U postgres -c 'CREATE DATABASE l2beat_local' 
-psql -h localhost postgres -U postgres -c 'CREATE DATABASE l2beat_test' 
